ed21570b94f2923427a9c4a7b692d1ad9a9373ce
TideDra <gearyzhang@outlook.com>
2026-02-28 10:37:36 +0000

    fix logging

--- src/zotero_arxiv_daily/reranker/local.py
+++ src/zotero_arxiv_daily/reranker/local.py
@@ -5,18 +5,19 @@ import numpy as np
 @register_reranker("local")
 class LocalReranker(BaseReranker):
     def get_similarity_score(self, s1: list[str], s2: list[str]) -> np.ndarray:
-        from transformers.utils import logging as transformers_logging
-        from huggingface_hub.utils import logging as hf_logging
-        from sentence_transformers import SentenceTransformer
-
-        transformers_logging.set_verbosity_error()
-        hf_logging.set_verbosity_error()
-        logging.getLogger("sentence_transformers").setLevel(logging.ERROR)
-        logging.getLogger("sentence_transformers.SentenceTransformer").setLevel(logging.ERROR)
-        logging.getLogger("transformers").setLevel(logging.ERROR)
-        logging.getLogger("huggingface_hub").setLevel(logging.ERROR)
-        logging.getLogger("huggingface_hub.utils._http").setLevel(logging.ERROR)
-        warnings.filterwarnings("ignore", category=FutureWarning)
+        if not self.config.executor.debug:
+            from transformers.utils import logging as transformers_logging
+            from huggingface_hub.utils import logging as hf_logging
+            from sentence_transformers import SentenceTransformer
+    
+            transformers_logging.set_verbosity_error()
+            hf_logging.set_verbosity_error()
+            logging.getLogger("sentence_transformers").setLevel(logging.ERROR)
+            logging.getLogger("sentence_transformers.SentenceTransformer").setLevel(logging.ERROR)
+            logging.getLogger("transformers").setLevel(logging.ERROR)
+            logging.getLogger("huggingface_hub").setLevel(logging.ERROR)
+            logging.getLogger("huggingface_hub.utils._http").setLevel(logging.ERROR)
+            warnings.filterwarnings("ignore", category=FutureWarning)
 
         encoder = SentenceTransformer(self.config.reranker.local.model, trust_remote_code=True)
         if self.config.reranker.local.encode_kwargs:
